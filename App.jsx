7d4cfd36a4f9261b098986edf7766012fe9b893e
jwni <jwni@itu.dk>
2023-05-02 02:03:08 +0200

    Added react-native-maps dependency & added boilerplate map to App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import { Text } from "react-native";
+import { Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 55.660893,
+  longitude: 12.591242,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+};
 
 export default function App() {
   return (
-    <SafeAreaView className="h-full bg-black ">
-      <Text className="text-white">din mor</Text>
-    </SafeAreaView>
+    <MapView
+      className="h-full w-full"
+    />
   );
 }
