51359d23f67cf008ea2c8d288cdbf568b356cc9c
Nicklas Lundsteen Jensen <nicklaslundsteen@gmail.com>
2023-05-02 22:30:58 +0200

    .env and stuff

--- App.jsx
+++ App.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import { Dimensions } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-
-const { width, height } = Dimensions.get("window");
-
-const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.02;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const INITIAL_POSITION = {
-  latitude: 55.660893,
-  longitude: 12.591242,
-  latitudeDelta: LATITUDE_DELTA,
-  longitudeDelta: LONGITUDE_DELTA,
-};
+import { Dimensions, View } from "react-native";
+import user from "./src/utils/firebase"
 
 export default function App() {
+  console.log(user);
   return (
-    <MapView
+    <View
       className="h-full w-full"
     />
   );
