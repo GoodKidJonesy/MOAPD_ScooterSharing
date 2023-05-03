62157e5b96d00b6791ef647032f6378da1c49906
entrl <martin.kataroski@gmail.com>
2023-05-03 19:42:39 +0200

    added db calls

--- App.jsx
+++ App.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from "react";
 import MapComponent from "./src/Components/MapComponent";
 import { SafeAreaView, View } from "react-native";
-import { getScooters, addUser, scooterRegistered, getHistoryOfUser } from "./src/utils/Firebase";
+import { getScooters, addUser, scooterRegistered, getHistoryOfUser, getUnreservedScootersIds, getRentUserHistory } from "./src/utils/Firebase";
 
 
 export default function App() {
+//   useEffect(() => {
+//     getScooters().then((res)=>{console.log(res)});
+// }, []);
+
   useEffect(() => {
-    getScooters().then((res)=>{console.log(res)});
-}, []);
+    getRentUserHistory(123);
+    // getUnreservedScootersIds()
+  })
   
   return (
     <SafeAreaView className="w-full h-full bg-black">
--- src/utils/Firebase.jsx
+++ src/utils/Firebase.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDoc, getDocs, doc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDoc, getDocs, doc, query, where, onSnapshot, QuerySnapshot } from "firebase/firestore";
 import {ref, set} from "firebase/database"
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -31,13 +31,42 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export const addUser = async(username, password, email) => {
-  const docRef = await addDoc(collection(db, "users"), {
-    username: username,
-    password: password,
-    email: email,
-  });
-};
+
+// query
+
+// gets the ids of the unreserved scooters
+export const getUnreservedScootersIds = async() => {
+
+  const getUnReservedScooters = query(collection(db, 'scooters'), where('isReserved', '==', false))
+  const isReservedSnapshot = onSnapshot(getUnReservedScooters, (querySnapshot) =>{
+    const unReservedScooters = [];
+    querySnapshot.forEach((doc) => {
+        unReservedScooters.push(doc.data())
+    });
+    // console.log("scooterId: ", unReservedScooters.join(', '))
+    return unReservedScooters;
+   
+  }) 
+  return isReservedSnapshot
+}
+
+export const getRentUserHistory = async(userName) => {
+
+  const getHistoryQuery = query(collection(db, 'historyOfUser'), where('userID', '==', userName))
+  const getUserHistory = onSnapshot(getHistoryQuery, (querySnapshot) => {
+    const userHistoryArr = [];
+    querySnapshot.forEach((doc) => {
+      userHistoryArr.push(doc.data())
+    })
+    // console.log('user: ', userHistoryArr.join(', '))
+    return userHistoryArr;
+  })
+  // console.log(getUserHistory)
+  return getUserHistory;
+}
+
+
+// fetch from db
 
 export const getScooters = async() => {
   const scooterSnapshot = await getDocs(collection(db, "scooters"));
@@ -51,6 +80,15 @@ export const getUser = async(user) => {
   return userDocument.data();
 };
 
+export const getHistoryOfUser = async(userName) => {
+  const historySnapshot = await getDocs(collection(db, "historyOfUser"));
+  historySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+};
+
+//inserts to db
+
 export const addScooterToHistory = async(scooterName, userName) => {
   const docRef = await addDoc(collection(db, "historyOfUser"), {
     scooterName: scooterName,
@@ -59,11 +97,12 @@ export const addScooterToHistory = async(scooterName, userName) => {
   });
 };
 
-export const getHistoryOfUser = async(userName) => {
-   const querySnapshot = await getDocs(collection(db, "historyOfUser"));
-   querySnapshot.forEach((doc) => {
-     console.log(doc.data());
-   });
+export const addUser = async(username, password, email) => {
+  const docRef = await addDoc(collection(db, "users"), {
+    username: username,
+    password: password,
+    email: email,
+  });
 };
 
-
+//update
